272fede07a714552568465e96a4dfe6ef520b439
reshreshus <reshreshus@gmail.com>
2019-11-22 00:48:24 +0300

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Navbar, ProductList, Details, Cart, Default, PageNotFound } from './components';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <React.Fragment>
+      <Navbar/>
+      <Switch>
+        <Route path="/" exact component={ProductList}/>
+        <Route path="/details" exact component={Details}/>
+        <Route path="/cart" exact component={Cart}/>
+        <Route  component={PageNotFound}/>
+      </Switch>
+    </React.Fragment>
   );
 }
 
